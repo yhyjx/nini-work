0a62e39ab04b98ad3a3a35f84a204a4434cf5afd
yuhao <iyuhaoo@126.com>
2024-06-11 17:14:18 +0800

    代码提交

--- js/myLang.js
+++ js/myLang.js
@@ -1,68 +1,68 @@
 const langList = {
   area1: {
     title: {
-      zh: "仙鹤岛",
+      cn: "仙鹤岛",
       en: "The Crane Island",
       jp: "仙鶴島",
     },
     subTitle: {
-      zh: "三面环水，一面临山，植被多样，灌木葱茏。",
+      cn: "三面环水，一面临山，植被多样，灌木葱茏。",
       en: "The Crane Island, embraced by the clear waters on three sides and the protective mountains on the fourth, is a verdant tapestry of flora, where shrubs thrive in abundance.",
       jp: "三面は水に囲まれ、一面は山に面しており、多様な植物が茂っており、低木が茂っています。",
     },
     intro: {
-      zh: "仙鹤岛，这个位于管理局西南4公里处的自然奇迹，被清澈的水域和连绵的山峦所环绕，是探索自然和进行生态旅游的理想之地。这里的土壤极为肥沃，孕育了一片片茂盛的植被。无论是参天大树，还是低矮的灌木，都在这里找到了它们生长的乐园。",
+      cn: "仙鹤岛，这个位于管理局西南4公里处的自然奇迹，被清澈的水域和连绵的山峦所环绕，是探索自然和进行生态旅游的理想之地。这里的土壤极为肥沃，孕育了一片片茂盛的植被。无论是参天大树，还是低矮的灌木，都在这里找到了它们生长的乐园。",
       en: "Crane Island, a natural marvel located just 4 kilometers southwest of the administration, is encircled by pristine waters and rolling hills, an ideal haven for nature exploration and eco-tourism. The soil here is incredibly fertile, nurturing lush vegetation. From towering trees to the humble shrubs, all find their sanctuary in this paradise.",
       jp: "管理局の南西4キロメートルに位置する仙鶴島は、自然の奇跡で、清澈な水域と連峰に囲まれており、自然を探求し、生態ツーリズムを楽しむ理想的な場所です。ここでの土壌は非常に肥沃で、茂密な植物が育まれています。大きな木も低い灌木も、ここで成長する楽園を見つけています。",
     },
   },
   area2: {
     title: {
-      zh: "蒙古黄榆林",
+      cn: "蒙古黄榆林",
       en: "The Mongolian Poplar Forest",
       jp: "モンゴルの黄榆林",
     },
     subTitle: {
-      zh: "亚洲最大的蒙古黄榆林，面积约50平方公里。",
+      cn: "亚洲最大的蒙古黄榆林，面积约50平方公里。",
       en: "The Mongolian Poplar Forest, spanning an impressive 50 square kilometers, stands as the largest of its kind in Asia, a testament to the resilience and grandeur of nature.",
       jp: "アジア最大のモンゴルの黄榆林で、面積は約50平方キロメートルです。",
     },
     intro: {
-      zh: "黄榆林以其独特的自然景观而闻名，这些坚韧的榆树见证了无数岁月的变迁，它们的存在本身就是对生命力的颂歌。黄榆林的生态环境极为丰富，这里是众多野生动物的栖息地，也是多样植被的乐园。在您的漫步中，或许能偶遇机警的野生动物，或是观察到稀有植物的生长状态。",
+      cn: "黄榆林以其独特的自然景观而闻名，这些坚韧的榆树见证了无数岁月的变迁，它们的存在本身就是对生命力的颂歌。黄榆林的生态环境极为丰富，这里是众多野生动物的栖息地，也是多样植被的乐园。在您的漫步中，或许能偶遇机警的野生动物，或是观察到稀有植物的生长状态。",
       en: "The Poplar Forest, renowned for its unique natural landscape, stands as a monument to the passage of time. These tenacious trees are a hymn to the enduring strength of life. The forest's ecosystem is rich and diverse, a haven for wildlife and a paradise for a variety of plants. As you stroll through, you might encounter alert wild animals or observe the growth of rare botanical species.",
       jp: "黄榆林は独特の自然の風景で知られており、これらの頑強な榆は無数の歳月の変遷を証言しており、その存在自体が生命力への賛歌です。黄榆林の生態環境は非常に豊かで、多くの野生動物の棲み家であり、多様な植物の楽園でもあります。散歩中に、機敏な野生動物に出会い、珍しい植物の成長状態を観察するかもしれません。",
     },
   },
   area3: {
     title: {
-      zh: "香海寺",
+      cn: "香海寺",
       en: "The Xianghai Temple",
       jp: "香海寺",
     },
     subTitle: {
-      zh: "遇见初识，香海禅寺，静听风海，铃音动听。",
+      cn: "遇见初识，香海禅寺，静听风海，铃音动听。",
       en: "The Xianghai Temple, upon first encounter, offers a serene experience, with the melodious chimes carried on the sea breeze, a symphony of spiritual tranquility.",
       jp: "出会ったばかりの香海禅寺で、風と海の音を静かに聞いて、鐘の音が心地よいです。",
     },
     intro: {
-      zh: "香海寺的建筑风格独树一帜，融合了藏传佛教与汉传佛教的艺术精华。金顶殿的高耸与辉煌，围墙上壁画的精致与生动，无不彰显着寺庙的庄严肃穆与文化深度。在这里，您可以感受到宁静与虔诚的氛围，听到悠扬的佛号声，与喇嘛们交流，深入了解佛教的智慧与教义。香海寺是一片寻求内心宁静和精神净化的净土。",
+      cn: "香海寺的建筑风格独树一帜，融合了藏传佛教与汉传佛教的艺术精华。金顶殿的高耸与辉煌，围墙上壁画的精致与生动，无不彰显着寺庙的庄严肃穆与文化深度。在这里，您可以感受到宁静与虔诚的氛围，听到悠扬的佛号声，与喇嘛们交流，深入了解佛教的智慧与教义。香海寺是一片寻求内心宁静和精神净化的净土。",
       en: "Xianghai Temple, with its distinctive architectural style, blends the artistic essence of Tibetan and Han Buddhism. The towering golden roof and the exquisite murals on the walls reflect the temple's solemnity and cultural depth. Here, you can experience an atmosphere of tranquility and devotion, hear the melodious chants, engage with the lamas, and delve into the wisdom and teachings of Buddhism. Xianghai Temple is a sanctuary for those seeking inner peace and spiritual purification.",
       jp: "香海寺の建築様式は独自で、チベット仏教と漢仏教の芸術の精髄を融合させています。金色の屋根の高さと輝き、壁の絵画の繊細さと生き生きとした様子は、寺院の荘厳さと文化的深さを表しています。ここでは、静かで虔敬な雰囲気を感じ、悠扬たる仏号の声を聞いて、ラマたちと交流し、仏教の知恵と教えを深く理解することができます。香海寺は、心の静寂と精神の浄化を求める清潔な土地です。",
     },
   },
   area4: {
     title: {
-      zh: "宣教中心",
+      cn: "宣教中心",
       en: "The Education Center",
       jp: "宣教センター",
     },
     subTitle: {
-      zh: "是向海自然保护区的微观缩影，体现了向海湿地特性，尤其各种动物栩栩如生。",
+      cn: "是向海自然保护区的微观缩影，体现了向海湿地特性，尤其各种动物栩栩如生。",
       en: "The Education Center, a microcosm of the Xianghai Nature Reserve, encapsulates the essence of the wetlands, with lifelike representations of the diverse fauna that call this place home.",
       jp: "向かい海の自然保護区の縮図であり、向かい海の湿地の特徴を体現しており、特に様々な動物が生き生きとしています。",
     },
     intro: {
-      zh: "宣教中心的外观设计巧妙地融合了自然元素，邀请您在绿树的环抱中，开始一段探索之旅。馆内精心策划的展览，将向海湿地的多样性与复杂性生动地展现在我们面前。这里陈列着众多动物标本，它们的姿态栩栩如生，仿佛随时会振翅高飞或潜入水中。",
+      cn: "宣教中心的外观设计巧妙地融合了自然元素，邀请您在绿树的环抱中，开始一段探索之旅。馆内精心策划的展览，将向海湿地的多样性与复杂性生动地展现在我们面前。这里陈列着众多动物标本，它们的姿态栩栩如生，仿佛随时会振翅高飞或潜入水中。",
       en: "The Education Center",
       jp: "宣教センター",
     },
